d5d5f3d9150230cea60f381c1cf72cb0dfa71ff3
Dallin Wright <dallinwright01@gmail.com>
2024-02-05 15:33:06 -0800

    adding files

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,60 @@
 import {Express, Request, Response} from 'express';
-
+import { v4 as uuidv4 } from 'uuid';
 
 import * as express from "express";
-import {connectToDatabase} from "./database";
+import {connectToDatabase} from "./config/database";
 import {Sequelize} from "sequelize-typescript";
+import User from "./models/users";
+import path from 'path';
 
 const app: Express =  express();
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
+// Configure Express to use EJS as the view engine
+app.set('view engine', 'ejs');
 
 export const dialect = "postgres";
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`
-        <form action="/submit" method="post">
-            <label for="username">Enter Name</label>
-            <input type="text" name="username" />
-            <input type="submit" value="Submit" />
-        </form>
-    `);
-});
 
-app.post('/submit', (req: Request, res: Response) => {
-    console.log(req.body.textbox);
+async function defineRoutes() {
+    const database: Sequelize = await connectToDatabase();
 
-    res.send('Received your text: ' + req.body.textbox);
-});
+    // about page
+    app.get('/users', function(req, res) {
+        res.render('src/views/users');
+    });
 
-app.listen(port, async () => {
-    let database: Sequelize = await connectToDatabase();
 
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    app.get('/', async (req: Request, res: Response) => {
+        // Refresh the users table
+        const users = await User.findAll();
+        res.render('pages/index', {users});
+    });
+
+    app.post('/submit', async (req: Request, res: Response) => {
+        try {
+            // Create a new user in the database
+            await User.create({
+                id: uuidv4(), // Generate a new UUIDv4
+                name: req.body.username,
+            });
+
+            // Redirect to the homepage
+            res.redirect('/');
+        } catch (error) {
+            // Handle errors
+            console.error('Error creating user:', error);
+            res.status(500).json({ error: 'Error creating user' });
+        }
+    });
+}
+defineRoutes().then(() => {
+    app.listen(port, () => {
+        console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+}).catch((error) => {
+    console.error("Unable to define routes:", error);
+    process.exit(1);
 });
--- src/models/users.ts
+++ src/models/users.ts
@@ -5,9 +5,8 @@ import { Model, Table, Column, DataType } from "sequelize-typescript";
 })
 class User extends Model {
     @Column({
-        type: DataType.INTEGER,
+        type: DataType.UUIDV4,
         primaryKey: true,
-        autoIncrement: true,
         field: "id"
     })
     id: number;
@@ -17,6 +16,22 @@ class User extends Model {
         field: "name"
     })
     name: string;
+
+    @Column({
+        type: DataType.DATE,
+        field: "createdAt",
+        defaultValue: DataType.NOW,
+        allowNull: false
+    })
+    createdAt: Date;
+
+    @Column({
+        type: DataType.DATE,
+        field: "updatedAt",
+        defaultValue: DataType.NOW,
+        allowNull: false
+    })
+    updatedAt: Date;
 }
 
 export default User;
