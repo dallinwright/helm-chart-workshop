3eb6c2a50f229171692fedb2c0d5dbce3cee5a9f
Dallin Wright <dallinwright01@gmail.com>
2024-02-05 14:57:03 -0800

    flusing out files

---
+++ src/models/users.ts
@@ -0,0 +1,22 @@
+import { Model, Table, Column, DataType } from "sequelize-typescript";
+
+@Table({
+    tableName: "users",
+})
+class User extends Model {
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: "id"
+    })
+    id: number;
+
+    @Column({
+        type: DataType.STRING(255),
+        field: "name"
+    })
+    name: string;
+}
+
+export default User;
